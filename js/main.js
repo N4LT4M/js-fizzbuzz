4e2a098cbb8353c94712ee1278d6c4c096ccd50e
N4LT4M <m4tt3o.casale@gmail.com>
2023-07-04 16:44:13 +0200

    correzione e vera fine esercizio base

--- js/main.js
+++ js/main.js
@@ -28,7 +28,6 @@ for (let i = 1; i <= 100; i++) {
         console.log("Buzz")
     } else if (i % 3 == 0) {
         console.log("Fizz")
-    }
-    console.log(i);
+    } else (console.log(i));
 }
 
